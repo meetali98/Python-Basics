08ed1dc65d9b79bc2971cc320fa651823e358463
meetali98 <meetalimude@gamil.com>
2021-11-08 19:21:31 +0530

    changes

--- Hackerrank/Find-a-substring-in-string.py
+++
@@ -1,7 +0,0 @@
-def count_substring(string, sub_string):
-    l = []
-    for i in range(0, (len(string) - len(sub_string) + 1)):
-        if string[i:i + len(sub_string)] == sub_string:
-            l.append(1)
-
-    return sum(l)
